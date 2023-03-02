afc9019047038ec62c9348903633ea0605e814b7
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-03-02 10:27:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -53,35 +53,20 @@ app.post('/', async (req, res) => {
 
 
 
-
-app.post('/v2', async (req, res) => {
+app.post('/new', async (req, res) => {
   try {
-    const data = [
-      {
-        name: 'ishimwe robert',
-        birthdate: 'March 14, 2000',
-        birthplace: 'kigali',
-        occupation: 'Physicist',
-        achievements: 'Developed the theory of relativity and made groundbreaking contributions to the study of quantum mechanics. Awarded the Nobel Prize in Physics in 1921.',
-        deathdate: 'April 18, 1955',
-        deathplace: 'Princeton, New Jersey, USA'
-      }
-    ]
     const prompt = req.body.prompt;
 
     const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `${prompt}`,
-      temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
-      data: data
+      model: "gpt-3.5-turbo",
+       messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": `${prompt}`},
+    ]
     });
 
     res.status(200).send({
-      bot: response.data.choices[0].text
+      bot: response
     });
 
   } catch (error) {
