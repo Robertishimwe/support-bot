2aa3aa844c3d3acfc314cc0ac0e2d2a7e161a4b7
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-03-02 12:19:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ app.post('/new', async (req, res) => {
       model: "gpt-3.5-turbo",
        messages: [
          {role: "system", content: "your name is ishimwe"},
+          {role: "system", content: "Always add signature after response"},
          {role: "user", content: `answer asked question acting as technical support engineer called "ishimwe" working for microfocus. you have to sound as professional techinical support engineer. if a customer ask you about how to access microfocus ftp of ftp, give him this link "https://ftp-pro.houston.softwaregrp.com/mffts/home. please provide information about ftp only if you are asked to". question: ${prompt}`},
          {role: "system", content: "your name is ishimwe"}
                  ], 
