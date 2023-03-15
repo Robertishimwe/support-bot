05881f3a461ae4fc1243d0ccae02c8d88b95a9f3
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-03-15 14:03:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -57,11 +57,21 @@ app.post('/new', async (req, res) => {
   try {
     const prompt = req.body.prompt;
     
-     if(prompt.includes("FTP LINK") || prompt.includes("ftp link")) {
+    
+     if(prompt.toLowerCase().includes("FTP LINK") || prompt.includes("ftp link") || prompt.includes("access ftp")) {
        res.status(200).send({
       bot: "Link to Microfocus FTP ---> https://ftp-pro.houston.softwaregrp.com/mffts/home"
     });
       
+     if(prompt.toLowerCase().includes("Time zone") || prompt.toLowerCase().includes("Timezone")) {
+       res.status(200).send({
+      bot: "Link to Time buddy ---> https://www.worldtimebuddy.com"
+    });
+      
+     if(prompt.toLowerCase().includes("Knowledge base") || prompt.toLowerCase().includes("KB") || prompt.toLowerCase().includes("KM")) {
+       res.status(200).send({
+      bot: "Link to KNOWLEDGE BASE ---> https://portal.microfocus.com/s/customportalsearch?language=en_US"
+    });
     
     }
 
