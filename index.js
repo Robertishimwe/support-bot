ea2ef6bd5852302d05386696cd2e4efd9bce8ed7
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-03-17 10:09:33 +0000

    refactored

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
-import { Configuration, OpenAIApi } from 'openai'
+import { createChatCompletion } from './services/openai'
+//import { Configuration, OpenAIApi } from 'openai'
 
-dotenv.config()
+// dotenv.config()
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
 
-const openai = new OpenAIApi(configuration);
+// const openai = new OpenAIApi(configuration);
 
 const app = express()
 app.use(cors())
@@ -45,29 +46,34 @@ app.post('/new', async (req, res) => {
       });
     }
 
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-       messages: [
-         {role: "system", content: "your name is ishimwe"},
-         {role: "system", content: `Always add signature after response`},
-         {role: "system", content: `if you are asked about FTP give this link "https://ftp-pro.houston.softwaregrp.com/mffts/home"`},
-         {role: "system", content: `if you are asked about Time zone give this link "https://www.worldtimebuddy.com"`},
-         {role: "system", content: `if you are asked about Knowledge base or KB or KM give this link "https://portal.microfocus.com/s/customportalsearch?language=en_US"`},
-         {role: "user", content: `${prompt}`},
-         //{role: "user", content: `answer asked question acting as technical support engineer called "ishimwe" working for microfocus. you have to sound as professional techinical support engineer. if a customer ask you about how to access microfocus ftp of ftp, give him this link "https://ftp-pro.houston.softwaregrp.com/mffts/home. please provide information about ftp only if you are asked to". question: ${prompt}`},
-         {role: "system", content: "your name is ishimwe"}
-       ], 
-    });
 
-    res.status(200).send({
-      bot: response.data.choices[0].message.content
-    });
 
-    
-    if(prompt.includes("FTP") || prompt.includes("fpt")) {
-      Alert(`Link to Microfocus FTP ---> https://ftp-pro.houston.softwaregrp.com/mffts/home`)
-      console.log(`Link to Microfocus FTP ---> https://ftp-pro.houston.softwaregrp.com/mffts/home`)
-    }
+
+    const response = await createChatCompletion(prompt)
+    console.log(response)
+    return res.status(200).send(response);
+
+
+
+    // const response = await openai.createChatCompletion({
+    //   model: "gpt-3.5-turbo",
+    //    messages: [
+    //      {role: "system", content: "your name is ishimwe"},
+    //      {role: "system", content: `Always add signature after response`},
+    //      {role: "system", content: `if you are asked about FTP give this link "https://ftp-pro.houston.softwaregrp.com/mffts/home"`},
+    //      {role: "system", content: `if you are asked about Time zone give this link "https://www.worldtimebuddy.com"`},
+    //      {role: "system", content: `if you are asked about Knowledge base or KB or KM give this link "https://portal.microfocus.com/s/customportalsearch?language=en_US"`},
+    //      {role: "user", content: `${prompt}`},
+    //      //{role: "user", content: `answer asked question acting as technical support engineer called "ishimwe" working for microfocus. you have to sound as professional techinical support engineer. if a customer ask you about how to access microfocus ftp of ftp, give him this link "https://ftp-pro.houston.softwaregrp.com/mffts/home. please provide information about ftp only if you are asked to". question: ${prompt}`},
+    //      {role: "system", content: "your name is ishimwe"}
+    //    ], 
+    // });
+
+    // res.status(200).send({
+    //   bot: response.data.choices[0].message.content
+    // });
+
+   
     
   } catch (error) {
     console.error(error)
