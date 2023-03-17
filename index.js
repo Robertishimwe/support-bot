b9b5cd7afde4eaf6d6a113ac7ec61aa478f98827
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-03-17 11:38:44 +0000

    refactored

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 
-import routes from './routes';
+import routes from './routes.js';
 import {createChatCompletion} from './services/openai.js'
 
 
--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import chatControllers from '../controller/chatController'
+import chatControllers from '../controller/chatController.js'
 
 // import AppointmentController from '../controller/appointmentBooking'
 
