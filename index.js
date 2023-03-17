88a9e99eabf1ac14b4ac6f5de997992fe17f617f
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-03-17 11:41:30 +0000

    refactored

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 
-import routes from './routes.js';
+import routes from './routes';
 import {createChatCompletion} from './services/openai.js'
 
 
