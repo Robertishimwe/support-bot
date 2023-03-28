53245f39e09a77552b2d4f413ae184b7985c9e29
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-03-28 07:16:06 +0000

    reversed logic

--- services/commandService.js
+++ services/commandService.js
@@ -17,36 +17,65 @@
 
 // }
 
+// import Command from '../model/commands.js';
+
+// const createCommand = ({ command, value }) => {
+//   const newCommand = new Command({ command, value });
+
+//   return new Promise((resolve, reject) => {
+//     newCommand.save((err, savedCommand) => {
+//       if (err) {
+//         reject({ error: true, err });
+//       } else {
+//         console.log('Command saved successfully:', savedCommand);
+//         resolve({ error: false, data: savedCommand });
+//       }
+//     });
+//   });
+// };
+
+// const deleteCommand = (commandId) => {
+//   return new Promise((resolve, reject) => {
+//     Command.findByIdAndDelete(commandId, (err, deletedCommand) => {
+//       if (err) {
+//         reject({ error: true, err });
+//       } else if (!deletedCommand) {
+//         reject({ error: true, message: "Command not found" });
+//       } else {
+//         console.log('Command deleted successfully:', deletedCommand);
+//         resolve({ error: false, data: deletedCommand });
+//       }
+//     });
+//   });
+// };
+
+// export { deleteCommand, createCommand }
+
 import Command from '../model/commands.js';
 
-const createCommand = ({ command, value }) => {
+const createCommand = async ({ command, value }) => {
   const newCommand = new Command({ command, value });
 
-  return new Promise((resolve, reject) => {
-    newCommand.save((err, savedCommand) => {
-      if (err) {
-        reject({ error: true, err });
-      } else {
-        console.log('Command saved successfully:', savedCommand);
-        resolve({ error: false, data: savedCommand });
-      }
-    });
-  });
+  try {
+    const savedCommand = await newCommand.save();
+    console.log('Command saved successfully:', savedCommand);
+    return { error: false, data: savedCommand };
+  } catch (err) {
+    return { error: true, err };
+  }
 };
 
-const deleteCommand = (commandId) => {
-  return new Promise((resolve, reject) => {
-    Command.findByIdAndDelete(commandId, (err, deletedCommand) => {
-      if (err) {
-        reject({ error: true, err });
-      } else if (!deletedCommand) {
-        reject({ error: true, message: "Command not found" });
-      } else {
-        console.log('Command deleted successfully:', deletedCommand);
-        resolve({ error: false, data: deletedCommand });
-      }
-    });
-  });
+const deleteCommand = async (commandId) => {
+  try {
+    const deletedCommand = await Command.findByIdAndDelete(commandId);
+    if (!deletedCommand) {
+      return { error: true, message: "Command not found" };
+    }
+    console.log('Command deleted successfully:', deletedCommand);
+    return { error: false, data: deletedCommand };
+  } catch (err) {
+    return { error: true, err };
+  }
 };
 
-export { deleteCommand, createCommand }
+export { deleteCommand, createCommand };
