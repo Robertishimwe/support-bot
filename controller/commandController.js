b0d6f8fe995893a065651adf7b71b9f90f680f47
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-03-28 07:36:43 +0000

    reversed logic

--- controller/commandController.js
+++ controller/commandController.js
@@ -7,8 +7,7 @@ class commandControllers {
     static createCommand = async (req, res) => {
 
         const {command, value} = req.body;
-        console.log({command, value})
-        const response = await createCommand(command,value)
+        const response = await createCommand(command.toLowerCase(),value)
         console.log(response)
         return res.status(200).send(response)
 
