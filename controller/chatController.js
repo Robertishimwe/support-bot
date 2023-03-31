33618d553ffc610d197c3139f14141839e4942bf
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-03-31 15:40:34 +0000

    matching function

--- controller/chatController.js
+++ controller/chatController.js
@@ -1,3 +1,4 @@
+import {deleteCommand, createCommand, findComand} from '../services/commandService.js'
 import { createChatCompletion } from '../services/openai.js';
 
 class chatControllers {
@@ -7,31 +8,61 @@ class chatControllers {
 
         try {
     
-            const prompt = req.body.prompt;
+            const text = req.body.prompt;
+
+        const wordsArray = await findComand()
+
+
+            const findMatchingWord = (text, wordsArray)=> {
+              const words = text.split(' '); // split text into individual words
+              for (let i = 0; i < words.length; i++) {
+                if (wordsArray.includes(words[i])) {
+                  console.log(words[i])
+                  return words[i]; // return the matching word
+                }
+              }
+              console.log(false)
+              return false; // return false if no match is found
+            }
+            
+
+
+
+
+
+
+
+
+
+
+
+            // if(await findComand({command: prompt})){
+
+            // }else{}
         
             
-             if(prompt.toLowerCase().includes("FTP LINK") || prompt.includes("ftp link") || prompt.includes("access ftp")) {
-              return res.status(200).send({
-              bot: "Link to Microfocus FTP ---> https://ftp-pro.houston.softwaregrp.com/mffts/home"
-            });
-             }
+            //  if(prompt.toLowerCase().includes("FTP LINK") || prompt.includes("ftp link") || prompt.includes("access ftp")) {
+            //   return res.status(200).send({
+            //   bot: "Link to Microfocus FTP ---> https://ftp-pro.houston.softwaregrp.com/mffts/home"
+            // });
+            //  }
               
-             if(prompt.toLowerCase().includes("Time zone") || prompt.toLowerCase().includes("Timezone")) {
-              return res.status(200).send({
-              bot: "Link to Time buddy ---> https://www.worldtimebuddy.com"
-            });
-             }
+            //  if(prompt.toLowerCase().includes("Time zone") || prompt.toLowerCase().includes("Timezone")) {
+            //   return res.status(200).send({
+            //   bot: "Link to Time buddy ---> https://www.worldtimebuddy.com"
+            // });
+            //  }
               
-             if(prompt.toLowerCase().includes("Knowledge base") || prompt.toLowerCase().includes("KB") || prompt.toLowerCase().includes("KM")) {
-               return res.status(200).send({
-                bot: "Link to KNOWLEDGE BASE ---> https://portal.microfocus.com/s/customportalsearch?language=en_US"
-              });
-            }
+            //  if(prompt.toLowerCase().includes("Knowledge base") || prompt.toLowerCase().includes("KB") || prompt.toLowerCase().includes("KM")) {
+            //    return res.status(200).send({
+            //     bot: "Link to KNOWLEDGE BASE ---> https://portal.microfocus.com/s/customportalsearch?language=en_US"
+            //   });
+            // }
         
         
         
         
-            const response = await createChatCompletion(prompt)
+            const response = await createChatCompletion(text)
             console.log(response)
             return res.status(200).send({bot: response});
         
