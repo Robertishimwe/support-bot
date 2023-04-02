c1b89c19b06c12d1887a789a84409020891249f4
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-04-02 09:09:09 +0000

    test-new-openai

--- controller/chatController.js
+++ controller/chatController.js
@@ -1,53 +1,61 @@
-import { createChatCompletion } from '../services/openai.js';
+import {
+  deleteCommand,
+  createCommand,
+  findComand,
+} from "../services/commandService.js";
+import { createChatCompletion } from "../services/openai.js";
 
 class chatControllers {
+  // chat controller
+  static chat = async (req, res) => {
+    try {
+      const text = req.body.prompt;
 
-    // chat controller
-    static chat = async (req, res) => {
+      const commandsArray = await findComand(); // use a more descriptive variable name
+      console.log("before extract", commandsArray);
+      // res.send(commandsArray)
 
-        try {
-    
-            const prompt = req.body.prompt;
-        
-            
-             if(prompt.toLowerCase().includes("FTP LINK") || prompt.includes("ftp link") || prompt.includes("access ftp")) {
-              return res.status(200).send({
-              bot: "Link to Microfocus FTP ---> https://ftp-pro.houston.softwaregrp.com/mffts/home"
-            });
-             }
-              
-             if(prompt.toLowerCase().includes("Time zone") || prompt.toLowerCase().includes("Timezone")) {
-              return res.status(200).send({
-              bot: "Link to Time buddy ---> https://www.worldtimebuddy.com"
-            });
-             }
-              
-             if(prompt.toLowerCase().includes("Knowledge base") || prompt.toLowerCase().includes("KB") || prompt.toLowerCase().includes("KM")) {
-               return res.status(200).send({
-                bot: "Link to KNOWLEDGE BASE ---> https://portal.microfocus.com/s/customportalsearch?language=en_US"
-              });
-            }
-        
-        
-        
-        
-            const response = await createChatCompletion(prompt)
-            console.log(response)
-            return res.status(200).send({bot: response});
-        
-        
-        
-           
-            
-          } catch (error) {
-            console.error(error)
-            res.status(500).send(error || 'Something went wrong');
-          }
-  
+      const extractCommands = (wordsArray) => {
+        if (!Array.isArray(wordsArray)) {
+          throw new Error("wordsArray is not an array");
+        }
+        const extractedCommands = wordsArray.map((obj) => obj.command);
+        console.log("after extract", extractedCommands);
+        return extractedCommands; // add a return statement to return the extracted commands
+      };
       
-    };
-  
 
-  }
-  
-  export default chatControllers;
+
+
+
+      
+        const findMatchingWord = (text, extractCommands)=> {
+          const words = text.split(' '); // split text into individual words
+          for (let i = 0; i < words.length; i++) {
+            if ( extractCommands.includes(words[i])) {
+              console.log({"matching words":words[i]})
+              return words[i]; // return the matching word
+            }
+          }
+          console.log(false)
+          return false; // return false if no match is found
+        }
+
+      if(findMatchingWord(text, extractCommands(commandsArray?.data))){
+        const myServeResponse = await findComand({command: findMatchingWord(text, extractCommands(commandsArray?.data))})
+        const response = await createChatCompletion(text);
+      // console.log(response);
+      return res.status(200).send({ dbres: myServeResponse.data[0].value, bot: response });
+        
+      }else{
+        console.log("foooooooooo")
+      }
+
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error || "Something went wrong");
+    }
+  };
+}
+
+export default chatControllers;
